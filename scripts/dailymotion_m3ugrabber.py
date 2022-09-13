dd0bc9ea6d40a0cb55066108783cbabd45d03d87
xcitv <110113306+xcitv@users.noreply.github.com>
2022-09-13 20:16:59 +0700

    Update dailymotion_m3ugrabber.py

--- scripts/dailymotion_m3ugrabber.py
+++ scripts/dailymotion_m3ugrabber.py
@@ -1,17 +1,5 @@
 #! /usr/bin/python3
 
-banner = r'''
-#########################################################################
-#      ____            _           _   __  __                           #
-#     |  _ \ _ __ ___ (_) ___  ___| |_|  \/  | ___   ___  ___  ___      #
-#     | |_) | '__/ _ \| |/ _ \/ __| __| |\/| |/ _ \ / _ \/ __|/ _ \     #
-#     |  __/| | | (_) | |  __/ (__| |_| |  | | (_) | (_) \__ \  __/     #
-#     |_|   |_|  \___// |\___|\___|\__|_|  |_|\___/ \___/|___/\___|     #
-#                   |__/                                                #
-#                                  >> https://github.com/benmoose39     #
-#########################################################################
-'''
-
 import requests
 import os
 import sys
@@ -23,7 +11,7 @@ if len(sys.argv) == 2:
                 'https' : sys.argv[1]
               }
 
-na = 'https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u'
+na = 'https://github.com/Xcpro24/chl/blob/main/assets/Eror404.mp4?raw=true'
 def grab(line):
     try:
         _id = line.split('/')[4]
@@ -48,6 +36,8 @@ def grab(line):
         print(m3u)
 
 print('#EXTM3U')
+print('#EXT-X-VERSION:3')
+print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000')
 print(banner)
 s = requests.Session()
 with open('../dailymotion_channel_info.txt') as f:
@@ -61,7 +51,6 @@ with open('../dailymotion_channel_info.txt') as f:
             grp_title = line[1].strip().title()
             tvg_logo = line[2].strip()
             tvg_id = line[3].strip()
-            print(f'\n#EXTINF:-1 group-title="{grp_title}" tvg-logo="{tvg_logo}" tvg-id="{tvg_id}", {ch_name}')
         else:
             grab(line)
         
